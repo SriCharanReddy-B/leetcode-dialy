80ba14e468c37a014890516af41e2f7ddcc52bb4
BERELLY SRI CHARAN REDDY <berellysricharan@gmail.com>
2026-06-22 22:28:31 +0530

    Time: 0 ms (100%), Space: 9.1 MB (15.37%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char, int> f;
+        for(char c:text)
+        f[c]++;
+
+        return min({f['b'],f['a'],f['l']/2,f['n'],f['o']/2});
+
+    }
+};
